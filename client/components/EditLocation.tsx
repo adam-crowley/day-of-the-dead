5cf9b375b5837ba6240780b1d001be6f1139cd8d
Adam Crowley <adamcrowley@gmail.com>
2023-09-18 16:42:21 +1200

    Add EditLocation component

---
+++ client/components/EditLocation.tsx
@@ -0,0 +1,13 @@
+import { useState, useEffect } from 'react'
+import { useParams } from 'react-router-dom'
+import { useForm, SubmitHandler } from 'react-hook-form'
+
+function EditLocation() {
+  return (
+    <>
+      <h2>Edit Location!</h2>
+    </>
+  )
+}
+
+export default EditLocation
--- client/routes.tsx
+++ client/routes.tsx
@@ -10,6 +10,7 @@ import Day from './components/Day'
 import Locations from './components/Locations'
 import AddEvent from './components/AddEvent'
 import EditEvent from './components/EditEvent'
+import EditLocation from './components/EditLocation'
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -18,6 +19,7 @@ const router = createBrowserRouter(
         <Route index element={<Home />} />
         <Route path="schedule/:day" element={<Day />} />
         <Route path="locations" element={<Locations />} />
+        <Route path="locations/:id/edit" element={<EditLocation />} />
         <Route path="events/add/:day" element={<AddEvent />} />
         <Route path="events/:id/edit" element={<EditEvent />} />
       </Route>
