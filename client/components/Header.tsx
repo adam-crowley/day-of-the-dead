4743d689962542e34d735123cfcc67e5df8663ca
Adam Crowley <adamcrowley@gmail.com>
2023-09-20 11:42:33 +1200

    Style banner on Header component

--- client/components/Header.tsx
+++ client/components/Header.tsx
@@ -2,10 +2,14 @@ import { Link } from 'react-router-dom'
 
 function Header() {
   return (
-    <header className="flex flex-wrap justify-center bg-dd-dark-purple">
-      <Link to="/" className="flex w-1/2">
-        <img src="/images/skull.png" alt="skull" className="w-1/2" />
-        <h1 className="flex flex-col justify-center w-1/2 text-left font-serif italic text-7xl text-dd-yellow">
+    <header className="flex flex-wrap justify-center py-10 bg-dd-dark-purple bg-gradient-to-b from-transparent from-40% to-dd-light-purple/30">
+      <Link to="/" className="flex w-6/12 header__link">
+        <img
+          src="/images/skull.png"
+          alt="skull"
+          className="self-center w-6/12 pr-10"
+        />
+        <h1 className="flex flex-col justify-center w-6/12 text-left font-serif italic text-7xl text-dd-yellow">
           Day of<br></br> the Dead<br></br>{' '}
           <span className="text-dd-gold">2024</span>
         </h1>
--- client/components/Nav.tsx
+++ client/components/Nav.tsx
@@ -2,13 +2,25 @@ import { Link } from 'react-router-dom'
 
 function Nav() {
   return (
-    <nav className="bg-dd-yellow">
-      <Link to="/schedule/friday">friday</Link>
-      <Link to="/schedule/saturday">saturday</Link>
-      <Link to="/schedule/sunday">sunday</Link>
-      <Link className="nav" to="/locations">
-        view locations
-      </Link>
+    <nav className="flex justify-center relative z-10 py-5 bg-dd-yellow">
+      <ul className="flex text-center font-serif text-2xl">
+        <li className="">
+          <Link to="/schedule/friday" className="text-dd-dark-purple">
+            Friday
+          </Link>
+        </li>
+        <li>
+          <Link to="/schedule/saturday">Saturday</Link>
+        </li>
+        <li>
+          <Link to="/schedule/sunday">Sunday</Link>
+        </li>
+        <li className="absolute right-0">
+          <Link className="nav" to="/locations">
+            view locations
+          </Link>
+        </li>
+      </ul>
     </nav>
   )
 }
