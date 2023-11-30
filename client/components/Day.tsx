3a37e77beb1c9c41f3867d28aeca2e36b7946fa9
Adam Crowley <adamcrowley@gmail.com>
2023-12-01 11:47:11 +1300

    Add animation to Day and Locations components

--- client/components/Day.tsx
+++ client/components/Day.tsx
@@ -1,6 +1,6 @@
-import { useState, useEffect, useRef, useLayoutEffect } from 'react'
+import { useState, useEffect } from 'react'
 import { useParams, Link } from 'react-router-dom'
-import { gsap } from 'gsap'
+import { motion } from 'framer-motion'
 
 import { getEventsByDay } from '../apis/api'
 import { EventWithLocation } from '../../models/event'
@@ -8,7 +8,6 @@ import { EventWithLocation } from '../../models/event'
 function Day() {
   const { day } = useParams()
   const [events, setEvents] = useState([])
-  const comp = useRef<HTMLDivElement | null>(null)
 
   useEffect(() => {
     const eventsByDay = async () => {
@@ -19,77 +18,65 @@ function Day() {
     eventsByDay()
   }, [day])
 
-  useLayoutEffect(() => {
-    const ctx = gsap.context(() => {
-      gsap.to('.fade-left', {
-        width: 0,
-        duration: 0.5,
-        ease: 'power2.inOut',
-      })
-      gsap.to('.card', {
-        opacity: 1,
-        duration: 0.5,
-        stagger: 0.1,
-        ease: 'power2.inOut',
-      })
-      gsap.to('.btn', {
-        opacity: 1,
-        duration: 0.5,
-        stagger: 0.1,
-        ease: 'power2.inOut',
-      })
-    }, comp)
-    return () => ctx.revert() // cleanup
-  })
-
   return (
     <>
-      <div ref={comp}>
-        <div className="flex justify-between relative mb-10">
-          <h2 className="relative text-center font-serif text-5xl text-dd-yellow italic">
-            <span className="fade-left bg-dd-dark-purple"></span>
-            Events on <span className="capitalize">{day}</span>
-          </h2>
+      <div className="flex justify-between relative mb-10">
+        <h2 className="relative text-center font-serif text-5xl text-dd-yellow italic">
+          <motion.span
+            key={day}
+            animate={{ width: 0 }}
+            transition={{ ease: 'easeInOut', duration: 0.5 }}
+            className="fade-left bg-dd-dark-purple"
+          ></motion.span>
+          Events on <span className="capitalize">{day}</span>
+        </h2>
+        <motion.div
+          animate={{ opacity: 1 }}
+          transition={{ ease: 'easeInOut', duration: 0.4 }}
+          className="opacity-0"
+        >
           <Link
-            className="btn self-center rounded-md border border-dd-yellow/50 hover:border-dd-yellow font-serif text-dd-gold hover:text-dd-yellow px-10 py-2 opacity-0"
+            className="btn self-center rounded-md border border-dd-yellow/50 hover:border-dd-yellow font-serif text-dd-gold hover:text-dd-yellow px-10 py-2"
             to={`/events/add/${day}`}
           >
             Add event
           </Link>
-        </div>
-
-        <ul className="flex flex-wrap flex-row gap-[5%] gap-y-10">
-          {events.map((event: EventWithLocation) => (
-            <li
-              key={event.id}
-              className={`card card--${event.locationId} flex flex-col justify-between w-[30%] p-7 border border-dd-yellow/50 text-dd-yellow opacity-0`}
-            >
-              <div>
-                <h3 className="mt-28 mb-5 pt-5 text-dd-gold font-serif text-xl">
-                  {event.eventName}
-                </h3>
-                <div className="mb-5">
-                  <p className="">
-                    Location:{' '}
-                    <span className="text-dd-gold">{event.locationName}</span>
-                  </p>
-                  <p>
-                    Time: <span className="text-dd-gold">{event.time}</span>
-                  </p>
-                </div>
-                <p className="mb-5">{event.description}</p>
-              </div>
-
-              <Link
-                to={`/events/${event.id}/edit`}
-                className="block w-full rounded-md border border-dd-yellow/50  hover:border-dd-yellow font-serif text-center text-dd-gold hover:text-dd-yellow px-10 py-2"
-              >
-                Edit event
-              </Link>
-            </li>
-          ))}
-        </ul>
+        </motion.div>
       </div>
+
+      <ul className="flex flex-wrap flex-row gap-[5%] gap-y-10">
+        {events.map((event: EventWithLocation) => (
+          <motion.li
+            key={event.id}
+            animate={{ opacity: 1 }}
+            transition={{ ease: 'easeInOut', duration: 0.4 }}
+            className={`card card--${event.locationId} flex flex-col justify-between w-[30%] p-7 border border-dd-yellow/50 text-dd-yellow opacity-0`}
+          >
+            <div>
+              <h3 className="mt-28 mb-5 pt-5 text-dd-gold font-serif text-xl">
+                {event.eventName}
+              </h3>
+              <div className="mb-5">
+                <p className="">
+                  Location:{' '}
+                  <span className="text-dd-gold">{event.locationName}</span>
+                </p>
+                <p>
+                  Time: <span className="text-dd-gold">{event.time}</span>
+                </p>
+              </div>
+              <p className="mb-5">{event.description}</p>
+            </div>
+
+            <Link
+              to={`/events/${event.id}/edit`}
+              className="block w-full rounded-md border border-dd-yellow/50  hover:border-dd-yellow font-serif text-center text-dd-gold hover:text-dd-yellow px-10 py-2"
+            >
+              Edit event
+            </Link>
+          </motion.li>
+        ))}
+      </ul>
     </>
   )
 }
--- client/components/Locations.tsx
+++ client/components/Locations.tsx
@@ -1,13 +1,12 @@
 import { useState, useEffect, useRef, useLayoutEffect } from 'react'
 import { Link } from 'react-router-dom'
-import { gsap } from 'gsap'
+import { motion } from 'framer-motion'
 
 import { getAllLocations } from '../apis/api'
 import { Location } from '../../models/location'
 
 function Locations() {
   const [locations, setLocations] = useState([])
-  const comp = useRef<HTMLDivElement | null>(null)
 
   useEffect(() => {
     const allLocations = async () => {
@@ -16,52 +15,39 @@ function Locations() {
     allLocations()
   }, [])
 
-  useLayoutEffect(() => {
-    const ctx = gsap.context(() => {
-      gsap.to('.fade-left', {
-        width: 0,
-        duration: 0.5,
-        ease: 'power2.inOut',
-      })
-      gsap.to('.card', {
-        opacity: 1,
-        duration: 0.5,
-        stagger: 0.1,
-        ease: 'power2.inOut',
-      })
-    }, comp)
-    return () => ctx.revert() // cleanup
-  })
-
   return (
     <>
-      <div ref={comp}>
-        <h2 className="relative mb-10 font-serif text-5xl text-dd-yellow italic">
-          <span className="fade-left bg-dd-dark-purple"></span>
-          Locations
-        </h2>
-        <ul className="flex flex-wrap flex-row gap-[5%] gap-y-10">
-          {locations.map((location: Location) => (
-            <li
-              key={location.id}
-              className={`card card--${location.id} flex flex-col justify-between w-[47.5%] p-7 border border-dd-yellow/50 text-dd-yellow opacity-0`}
+      <h2 className="relative mb-10 font-serif text-5xl text-dd-yellow italic">
+        <motion.span
+          animate={{ width: 0 }}
+          transition={{ ease: 'easeInOut', duration: 0.5 }}
+          className="fade-left bg-dd-dark-purple"
+        ></motion.span>
+        Locations
+      </h2>
+      <ul className="flex flex-wrap flex-row gap-[5%] gap-y-10">
+        {locations.map((location: Location) => (
+          <motion.li
+            key={location.id}
+            animate={{ opacity: 1 }}
+            transition={{ ease: 'easeInOut', duration: 0.4 }}
+            className={`card card--${location.id} flex flex-col justify-between w-[47.5%] p-7 border border-dd-yellow/50 text-dd-yellow opacity-0`}
+          >
+            <div>
+              <h3 className="mt-28 mb-5 pt-5 text-dd-gold font-serif text-xl">
+                {location.name}
+              </h3>
+              <p className="mb-5">{location.description}</p>
+            </div>
+            <Link
+              to={`/locations/${location.id}/edit`}
+              className="block w-full rounded-md border border-dd-yellow/50  hover:border-dd-yellow font-serif text-center text-dd-gold hover:text-dd-yellow px-10 py-2"
             >
-              <div>
-                <h3 className="mt-28 mb-5 pt-5 text-dd-gold font-serif text-xl">
-                  {location.name}
-                </h3>
-                <p className="mb-5">{location.description}</p>
-              </div>
-              <Link
-                to={`/locations/${location.id}/edit`}
-                className="block w-full rounded-md border border-dd-yellow/50  hover:border-dd-yellow font-serif text-center text-dd-gold hover:text-dd-yellow px-10 py-2"
-              >
-                Edit location
-              </Link>
-            </li>
-          ))}
-        </ul>
-      </div>
+              Edit location
+            </Link>
+          </motion.li>
+        ))}
+      </ul>
     </>
   )
 }
