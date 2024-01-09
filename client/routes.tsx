7aa36b8a2e01fd62915987d9774cb413e664ff7f
Adam Crowley <adamcrowley@gmail.com>
2024-01-09 17:01:08 +1300

    Update default route to be 'schedule/friday'

--- client/routes.tsx
+++ client/routes.tsx
@@ -2,10 +2,10 @@ import {
   createBrowserRouter,
   createRoutesFromElements,
   Route,
+  Navigate,
 } from 'react-router-dom'
 
 import App from './components/App'
-import Home from './components/Home'
 import Day from './components/Day'
 import Locations from './components/Locations'
 import AddEvent from './components/AddEvent'
@@ -15,8 +15,8 @@ import EditLocation from './components/EditLocation'
 const router = createBrowserRouter(
   createRoutesFromElements(
     <>
+      <Route path="/" element={<Navigate to="/schedule/friday" />} />
       <Route path="/" element={<App />}>
-        <Route index element={<Home />} />
         <Route path="schedule/:day" element={<Day />} />
         <Route path="locations" element={<Locations />} />
         <Route path="locations/:id/edit" element={<EditLocation />} />
