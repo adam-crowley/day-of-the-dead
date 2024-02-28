95af1d71e503c53967f81cca00ee23c51bd05e08
Adam Crowley <adamcrowley@gmail.com>
2024-02-28 14:44:30 +1300

    Add routes to server/index file

--- server/index.ts
+++ server/index.ts
@@ -1,8 +1,30 @@
-import server from './server'
+import { join } from 'node:path'
+import express from 'express'
+import cors, { CorsOptions } from 'cors'
+
+import locations from './routes/locations'
+import schedule from './routes/schedule'
+import events from './routes/events'
+
+const server = express()
+
+server.use(express.json())
+
+server.get('/', (req, res) =>
+  res.status(200).json({ message: 'Day of the Dead server' })
+)
+server.use('/api/v1/locations', locations)
+server.use('/api/v1/schedule', schedule)
+server.use('/api/v1/events', events)
+server.use('/api/v1/*', (req, res) => res.sendStatus(404))
+
+server.use(express.static(join(__dirname, './public')))
+server.use(cors('*' as CorsOptions))
 
 const port = process.env.PORT || 3000
 
 server.listen(port, function () {
-  // eslint-disable-next-line no-console
   console.log('Listening on port', port)
 })
+
+export default server
--- server/server.ts
+++ server/server.ts
@@ -1,24 +1,24 @@
-import { join } from 'node:path'
-import express from 'express'
-import cors, { CorsOptions } from 'cors'
+// import { join } from 'node:path'
+// import express from 'express'
+// import cors, { CorsOptions } from 'cors'
 
-import locations from './routes/locations'
-import schedule from './routes/schedule'
-import events from './routes/events'
+// import locations from './routes/locations'
+// import schedule from './routes/schedule'
+// import events from './routes/events'
 
-const server = express()
+// const server = express()
 
-server.use(express.json())
+// server.use(express.json())
 
-server.get('/', (req, res) =>
-  res.status(200).json({ message: 'Day of the Dead server' })
-)
-server.use('/api/v1/locations', locations)
-server.use('/api/v1/schedule', schedule)
-server.use('/api/v1/events', events)
-server.use('/api/v1/*', (req, res) => res.sendStatus(404))
+// server.get('/', (req, res) =>
+//   res.status(200).json({ message: 'Day of the Dead server' })
+// )
+// server.use('/api/v1/locations', locations)
+// server.use('/api/v1/schedule', schedule)
+// server.use('/api/v1/events', events)
+// server.use('/api/v1/*', (req, res) => res.sendStatus(404))
 
-server.use(express.static(join(__dirname, './public')))
-server.use(cors('*' as CorsOptions))
+// server.use(express.static(join(__dirname, './public')))
+// server.use(cors('*' as CorsOptions))
 
-export default server
+// export default server
