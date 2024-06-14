8cc70e5e1c8e87a91243f6ac2907826a292fb102
Adam Crowley <adamcrowley@gmail.com>
2024-06-14 14:35:06 +1200

    Add loading icon to Locations component

--- client/components/Day.tsx
+++ client/components/Day.tsx
@@ -9,7 +9,7 @@ import { EventWithLocation } from '../../models/event'
 function Day() {
   const { day } = useParams()
   const [events, setEvents] = useState([])
-  const [isLoading, setIsLoading] = useState<boolean>(true)
+  const [isLoading, setIsLoading] = useState(true)
 
   useEffect(() => {
     const eventsByDay = async () => {
--- client/components/Locations.tsx
+++ client/components/Locations.tsx
@@ -1,17 +1,21 @@
 import { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom'
 import { motion } from 'framer-motion'
+import { Vortex } from 'react-loader-spinner'
 
 import { getAllLocations } from '../apis/api'
 import { Location } from '../../models/location'
 
 function Locations() {
   const [locations, setLocations] = useState([])
+  const [isLoading, setIsLoading] = useState(true)
 
   useEffect(() => {
     try {
       const allLocations = async () => {
+        setIsLoading(true)
         setLocations(await getAllLocations())
+        setIsLoading(false)
       }
       allLocations()
     } catch (error) {
@@ -21,37 +25,58 @@ function Locations() {
 
   return (
     <>
-      <h2 className="relative mb-10 font-serif text-4xl md:text-5xl text-dd-yellow italic">
-        <motion.span
-          animate={{ width: 0 }}
-          transition={{ ease: 'easeInOut', duration: 0.4 }}
-          className="fade-left bg-dd-dark-purple"
-        ></motion.span>
-        Locations
-      </h2>
-      <ul className="flex flex-wrap flex-row gap-[5%] gap-y-10">
-        {locations.map((location: Location) => (
-          <motion.li
-            key={location.id}
-            animate={{ opacity: 1 }}
-            transition={{ ease: 'easeInOut', duration: 0.4 }}
-            className={`card card--${location.id} flex flex-col justify-between w-full sm:w-[47.5%] p-7 border border-dd-yellow/50 text-dd-yellow opacity-0`}
-          >
-            <div>
-              <h3 className="mt-28 mb-5 pt-5 text-dd-gold font-serif text-xl">
-                {location.name}
-              </h3>
-              <p className="mb-5">{location.description}</p>
-            </div>
-            <Link
-              to={`/locations/${location.id}/edit`}
-              className="block w-full rounded-md border border-dd-yellow/50  hover:border-dd-yellow font-serif text-center text-dd-gold hover:text-dd-yellow px-10 py-2"
-            >
-              Edit location
-            </Link>
-          </motion.li>
-        ))}
-      </ul>
+      {isLoading ? (
+        <Vortex
+          visible={true}
+          height="80"
+          width="80"
+          ariaLabel="vortex-loading"
+          wrapperStyle={{}}
+          wrapperClass="absolute left-[50%] top-[50%] translate-x-[-50%] translate-y-[-50%]"
+          colors={[
+            '#DA00FD',
+            '#FFC001',
+            '#F3EFD3',
+            '#DA00FD',
+            '#FFC001',
+            '#F3EFD3',
+          ]}
+        />
+      ) : (
+        <>
+          <h2 className="relative mb-10 font-serif text-4xl md:text-5xl text-dd-yellow italic">
+            <motion.span
+              animate={{ width: 0 }}
+              transition={{ ease: 'easeInOut', duration: 0.4 }}
+              className="fade-left bg-dd-dark-purple"
+            ></motion.span>
+            Locations
+          </h2>
+          <ul className="flex flex-wrap flex-row gap-[5%] gap-y-10">
+            {locations.map((location: Location) => (
+              <motion.li
+                key={location.id}
+                animate={{ opacity: 1 }}
+                transition={{ ease: 'easeInOut', duration: 0.4 }}
+                className={`card card--${location.id} flex flex-col justify-between w-full sm:w-[47.5%] p-7 border border-dd-yellow/50 text-dd-yellow opacity-0`}
+              >
+                <div>
+                  <h3 className="mt-28 mb-5 pt-5 text-dd-gold font-serif text-xl">
+                    {location.name}
+                  </h3>
+                  <p className="mb-5">{location.description}</p>
+                </div>
+                <Link
+                  to={`/locations/${location.id}/edit`}
+                  className="block w-full rounded-md border border-dd-yellow/50  hover:border-dd-yellow font-serif text-center text-dd-gold hover:text-dd-yellow px-10 py-2"
+                >
+                  Edit location
+                </Link>
+              </motion.li>
+            ))}
+          </ul>
+        </>
+      )}
     </>
   )
 }
